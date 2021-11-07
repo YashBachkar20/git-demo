ddaa0a033e45724bad554c797c2293fca0eaf07e
Yash Bachkar <yashbachkar20@gmail.com>
2021-11-07 23:11:17 +0530

    Added main pack and gitignore file

--- main.java
+++ main.java
@@ -5,6 +5,6 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("THis is a git demo, Updating this file.");
-        System.out.println("few more changes")
+        System.out.println("few more changes");
     }
 }
